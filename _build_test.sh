8ab4c35c4419bef5f794675025283cf4f53036b0
Joel Gilley <gilleyj@gmail.com>
2016-10-27 18:37:18 +0200

    Travis experimental building scripts...

---
+++ _build_test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+LINT=`which dockerlint`
+echo '#!/bin/sh' > ./_test.sh
+for f in */Dockerfile; do
+     echo "echo Processing ${f} ..." >> ./_test.sh
+     echo "${LINT} -f ${f}" >> ./_test.sh
+     echo 'let z=$z+$?' >> ./_test.sh
+done
+echo 'echo exiting with ${z} ...' >> ./_test.sh
+echo 'exit ${z}' >> ./_test.sh
+chmod ugo+x ./_test.sh
+
