5374cb7db384a67d7882ecf8053c7ffe8209ee9b
Joel Gilley <gilleyj@users.noreply.github.com>
2016-10-28 15:25:35 +0200

    Cleanup of initial containers added (#1)
    * Add SQS and S3 containers
    * Travis experimental building scripts...
    * Force bash
    * Travis error test
    * Clean up of FakeSQS

---
+++ alpine-fakesqs/container_confs/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+# entrypoint for container
+# ----------------------------------------------------------------------------
+set -e
+HOST_IP=`/bin/grep $HOSTNAME /etc/hosts | /usr/bin/cut -f1`
+if [ "$1" == "fakesqs" ]; then
+	echo "starting fakesqs with...."
+	echo /usr/bin/fake_sqs -v -o $HOST_IP -p 4568 --database /var/data/sqs/queues --log /proc/self/fd/1 --no-daemonize
+	/usr/bin/fake_sqs -v -o $HOST_IP -p 4568 --database /var/data/sqs/queues --log /proc/self/fd/1 --no-daemonize
+elif [ "$1" == "bash" ] || [ "$1" == "shell" ]; then
+	echo "starting /bin/bash with...."
+	/bin/bash --rcfile /etc/bashrc
+else
+	echo "Running something else"
+	exec "$@"
+fi
